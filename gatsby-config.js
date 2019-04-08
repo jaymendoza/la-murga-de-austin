ed381a0535206c8664211c58a556041bea182099
Jay Mendoza <jzpmendoza@gmail.com>
2019-04-08 02:08:44 -0500

    change site title, desc, and color scheme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `La Murga de Austin`,
+    description: `Hogar del Himno de La Murga`,
+    author: `@jaymendoza`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
