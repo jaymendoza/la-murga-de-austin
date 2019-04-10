0bfee5006ef2fd5f00a3f20f3baf89c2bddbc767
Jay Mendoza <jzpmendoza@gmail.com>
2019-04-10 02:43:34 -0500

    fix some code formatting

--- src/components/lyrics.js
+++ src/components/lyrics.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 class Lyrics extends React.Component {
   render() {
-    return <div class="lyrics">{this.props.lyrics.map(lyric => (
-      <div class="lyric">{lyric}</div>
-    ))}
-    </div>
+    return ( 
+      <div class="lyrics">
+        {this.props.lyrics.map(lyric => (
+          <div class="lyric">{lyric}</div>
+        ))}
+      </div>
+    )
   }
 }
 
--- src/components/menu.js
+++ src/components/menu.js
@@ -2,22 +2,22 @@ import React from 'react'
 import { Link } from "gatsby"
 
 const Menu = () => (
-    <div style={{
-      background: '#00B140',
-      paddingTop: '5px',
-      paddingBottom: '32px',
-     height: '2em'
-    }}>
+  <div style={{
+    background: '#00B140',
+    paddingTop: '5px',
+    paddingBottom: '32px',
+    height: '2em'
+  }}>
    
-      <ul style={{
-        listStyle: 'none',
-        display: 'flex',
-        justifyContent: 'center',
-      }}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/songlist">Songs</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+    <ul style={{
+      listStyle: 'none',
+      display: 'flex',
+      justifyContent: 'center',
+    }}>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/songlist">Songs</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
   </div>
 )
 
