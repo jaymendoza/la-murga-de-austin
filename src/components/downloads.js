317d411dad335c20a000b13bf05c811efd4e99c8
Jay Mendoza <jzpmendoza@gmail.com>
2019-04-11 04:06:12 -0500

    change download file var name to be more descriptive

--- src/components/downloads.js
+++ src/components/downloads.js
@@ -4,9 +4,9 @@ class Downloads extends React.Component {
   render() {
     return (
       <div>
-        {this.props.files.map(x => (
+        {this.props.files.map(file => (
           <div>
-            <a href={x.link}>{x.name}</a>
+            <a href={file.link}>{file.name}</a>
           </div>
         ))}
       </div>
